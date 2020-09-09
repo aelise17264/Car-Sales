f1927b7af0614600074babc25e5b156b9d1397ae
unknown <anna-brander>
2020-09-09 09:27:16 -0600

    onclicks added to feature buttons

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import React from 'react';
+import React, {useReducer} from 'react';
 
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
+import {initialState, reducer, initialFeatures} from './reducer/FeatureReducer'
 
 const App = () => {
-  const state = {
-    additionalPrice: 0,
-    car: {
-      price: 26395,
-      name: '2019 Ford Mustang',
-      image:
-        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
-      features: []
-    },
-   
-  };
+const [state, dispatch] = useReducer(initialState, reducer)
 
   return (
     <div className="boxes">
