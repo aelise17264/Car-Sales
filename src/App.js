be319bc99f82d1da3b3f60101a58764221fa9539
unknown <anna-brander>
2020-09-09 09:50:28 -0600

    mvp met

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React, {useReducer} from 'react';
-
+import {connect} from 'react-redux';
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
-import {initialState, reducer, initialFeatures} from './reducer/FeatureReducer'
+import {initialState, reducer} from './reducer/FeatureReducer';
+import {addFeature, removeFeature} from './reducer/Actions'
 
-const App = () => {
-const [state, dispatch] = useReducer(initialState, reducer)
+const App = (props) => {
+const {state, addFeature, removeFeature} = props
 
+console.log(state)
   return (
     <div className="boxes">
       <div className="box">
         <Header car={state.car} />
-        <AddedFeatures car={state.car} />
+        <AddedFeatures
+         car={state.car}
+        removeFeature={removeFeature} />
       </div>
       <div className="box">
-        <AdditionalFeatures additionalFeatures={state.additionalFeatures} />
-        <Total car={state.car} additionalPrice={state.additionalPrice} />
+        <AdditionalFeatures 
+        additionalFeatures={state.additionalFeatures}
+        addFeature={addFeature}
+        />
+        <Total car={state.car}
+         additionalPrice={state.additionalPrice} />
       </div>
     </div>
   );
 };
 
-export default App;
+const mapStatetoProps = state => {
+  return{
+    state
+  }
+}
+
+export default connect(mapStatetoProps, {addFeature, removeFeature})(App);
